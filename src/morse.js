d33d3a61c0bbf89a37435b4cfbc64a9f7d5799ee
Çetin <cetin200012@gmail.com>
2023-01-01 15:53:19 +0300

    started working on morse page

---
+++ src/morse.js
@@ -0,0 +1,7 @@
+// TextAreas
+const encodeString = document.querySelector('#encode-string');
+const decodeString = document.querySelector('#decode-string');
+
+// Buttons
+const encodeBtn = document.querySelector('#encode-btn');
+const decodeBtn = document.querySelector('#decode-btn');
